3c03c76b4e1098f5376273012ea37470078f9956
LebySla <wksh229@kmu.kr>
2021-09-24 11:49:49 +0900

    Sep 24th 2021

--- hw4.cpp
+++ hw4.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+typedef class Node{
+private:
+    int data;
+    Node * left;
+    Node * right;
+public:
+    ~Node(){
+        delete left;
+        delete right;
+    }
+}node;
+
+int main(int argc, char * argv[]){
+    return 0;
+}
